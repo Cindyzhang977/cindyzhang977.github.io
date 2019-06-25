8a5ec542da6773fe494d3643d629f87b1c9de13f
Cindy <cindyzhang977@gmail.com>
2019-06-25 11:14:07 -0700

    mobile menu toggle

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ const linkedin = new socialLink("#linkedin-img", "imgs/linkedin.png", "imgs/link
 const instagram = new socialLink("#instagram-img", "imgs/instagram.png", "imgs/instagram-hover.png");
 const github = new socialLink("#github-img", "imgs/github.png", "imgs/github-hover.png");
 const email = new socialLink("#email-img", "imgs/email.png", "imgs/email-hover.png");
+
+
+//toggle drop-down menu for mobile
+function toggleMenu() {
+  console.log("toggled menu");
+  var currDisplay = document.getElementById("main-menu").style.display;
+  if (currDisplay == "flex") {
+    document.getElementById("main-menu").style.display = "none";
+  } else {
+    document.getElementById("main-menu").style.display = "flex";
+  }
+}
+
+window.addEventListener("load", function(){
+  document.getElementById("main-menu").style.display = "none";
+  // console.log("glithc");
+  // if (document.getElementById("main-menu").style.display != "none"){
+  //   document.getElementById("main-menu").style.display = "none";
+  // }
+})
